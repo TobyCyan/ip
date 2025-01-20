fb72a9cea78d526eaa72221cc677b3254c12ec32
YongJunXi <ernestyongjunxi@gmail.com>
2025-01-20 11:13:20 +0800

    Add a response map and dedicated echo function
    A response hash map is used to map supported response types to their
    responses. This is to ensure that the responses needed can all be
    found in the same place, reducing the chances of unintended errors.
    
    An echo function is also added to ensure that all responses we are
    trying to give through the response manager are wrapped in the
    desired format.

--- src/main/java/Mei.java
+++ src/main/java/Mei.java
@@ -1,8 +1,17 @@
-public class Mei {
+import java.util.Scanner;
 
+public class Mei {
     public static void main(String[] args) {
         ResponseManager responseManager = new ResponseManager();
         responseManager.greetUser();
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+
+        while (!userInput.equals("bye")) {
+            responseManager.echo(userInput);
+            userInput = scanner.nextLine();
+        }
         responseManager.exitChat();
     }
 }
--- src/main/java/ResponseManager.java
+++ src/main/java/ResponseManager.java
@@ -1,16 +1,60 @@
+import java.util.HashMap;
+
 /**
  * This is the manager responsible for managing Mei's responses.
+ * Any calls to make Mei speak or make a response should be done via here.
  */
 public class ResponseManager {
+    private final HashMap<String, String[]> responseMap = new HashMap<>();
+
+    /**
+     * The constructor.
+     * Initializes a response map which maps the response type to its array of responses.
+     * The response map is given the final modifier as no modifications should be made upon initialization.
+     */
+    public ResponseManager() {
+        responseMap.put("Greeting", new String[] {"Hello! My name is Mei!", "What can I do for you?"});
+        responseMap.put("Exit", new String[] {"See you next time! :)"});
+    }
+
+    /**
+     * A function to redirect all incoming inputs to their respective functions.
+     * This should serve as a middle-man function between the user and the manager functions
+     * @param input The user input to redirect.
+     */
+    public void redirectInput(String input) {
+        echo(input);
+    }
+
+    /**
+     * A function that simply echos what is passed in.
+     * This function ensures that every response echoed is wrapped in divider lines.
+     * @param input The input string to echo.
+     */
+    public void echo(String input) {
+        dividerLine();
+        System.out.println(input);
+        dividerLine();
+    }
+
+    /**
+     * A function that echos everything in the given input array.
+     * This function ensures that all the responses echoed are wrapped in divider lines together.
+     * @param inputs The input array to echo.
+     */
+    public void echoLines(String[] inputs) {
+        dividerLine();
+        for (String input : inputs) {
+            System.out.println(input);
+        }
+        dividerLine();
+    }
 
     /**
      * Greets the user, pretty straight forward.
      */
     public void greetUser() {
-        dividerLine();
-        System.out.println("Hello! My name is Mei!");
-        System.out.println("What can I do for you?");
-        dividerLine();
+        echoLines(responseMap.get("Greeting"));
     }
 
     /**
@@ -18,9 +62,7 @@ public class ResponseManager {
      * This should be the final response of the interaction.
      */
     public void exitChat() {
-        dividerLine();
-        System.out.println("See you next time! :)");
-        dividerLine();
+        echoLines(responseMap.get("Exit"));
     }
 
     /**
